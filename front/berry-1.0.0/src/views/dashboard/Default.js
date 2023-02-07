d2ccf66f6b3e27de2d810deb84ac16e7162a9a62
Saiyam Vaid <saiyamvaid1997@gmail.com>
2023-02-07 18:07:08 +0530

    update

--- front/berry-1.0.0/src/views/dashboard/Default.js
+++ front/berry-1.0.0/src/views/dashboard/Default.js
@@ -14,7 +14,7 @@ const Dashboard = () => {
     return (
         <>
         <Form/>
-        <Grid container spacing={gridSpacing}>
+        {/* <Grid container spacing={gridSpacing}>
             <Grid item xs={12}>
                 <Grid container spacing={gridSpacing}>
                     <Grid item lg={4} md={6} sm={6} xs={12}>
@@ -45,7 +45,7 @@ const Dashboard = () => {
                     </Grid>
                 </Grid>
             </Grid>
-        </Grid>
+        </Grid> */}
         </>
     );
 };
